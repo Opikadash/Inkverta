fa444940ce93c274ce62293541e581c622d45c19
Opixato <130633108+Opikadash@users.noreply.github.com>
2026-03-22 17:18:12 +0530

    Delete comic-translator-app directory

--- comic-translator-app/backend/src/routes/ocr.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { extractText, extractTextFromMultiple } = require('../controllers/ocrController');
-
-router.post('/extract', extractText);
-router.post('/batch', extractTextFromMultiple);
-
-module.exports = router;
